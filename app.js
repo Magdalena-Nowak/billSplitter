4ed392c43ee31d6cbeb922e9fde27c866d57f24a
Magda <meg.nowak27@gmail.com>
2021-09-01 19:44:14 +0200

    finish js

--- app.js
+++ app.js
@@ -1,26 +1,21 @@
-const billValue = document.querySelector(".bill-value").value;
-const personNr = document.querySelector(".person-nr").value;
-const options = document.querySelectorAll("option");
+const options = document.querySelector("#tipPercentage");
 const warning = document.querySelector(".result__warning");
 const resultAmount = document.querySelector(".result__amount");
 const resultParagraph = document.querySelector(".result__text");
 const button = document.querySelector(".btn");
-let optionValue;
-
-options.forEach((option) => {
-  option.addEventListener("click", () => {
-    optionValue = option.getAttribute("value");
-  });
-});
 
 const calculations = () => {
+  const billValue = document.querySelector(".bill-value").value;
+  const personNr = document.querySelector(".person-nr").value;
+  const optionValue = options.value;
   if (billValue > 0 && personNr > 0 && optionValue) {
+    const result = (parseFloat(billValue) + (parseFloat(billValue) * optionValue) )/ parseFloat(personNr);
     warning.classList.remove("active");
-    const result = ((billValue + (billValue * optionValue)) / personNr);
     resultParagraph.classList.add("active");
-    resultAmount.textContent = result;
+    resultAmount.textContent = result.toFixed(2);
   } else {
     warning.classList.add("active");
+    resultParagraph.classList.remove('active');
   }
 };
 
